89098e80bd228281117410e5beb64c8975ee9576
Caspian Baska <email@caspian.computer>
2021-08-26 02:01:27 +1000

    test: fix mock driver

--- drivers/place/spec_helper_spec.cr
+++ drivers/place/spec_helper_spec.cr
@@ -1,2 +1,7 @@
-DriverSpecs.mock_driver "Place::SpecHelper" do
+require "placeos-driver/driver-specs/runner"
+require "placeos-driver/driver-specs/mock_driver"
+
+require "./spec_helper"
+
+DriverSpecs.mock_driver "SpecHelper" do
 end
